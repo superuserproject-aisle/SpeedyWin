49122952a2cba25d0359ea412d220bc6b0a2b81a
Fortnon <76496058+superuserproject-aisle@users.noreply.github.com>
2021-05-23 18:59:41 -0700

    Update SpeedyWin_NetworkFixer.bat

--- SpeedyWin_NetworkFixer.bat
+++ SpeedyWin_NetworkFixer.bat
@@ -1,6 +1,7 @@
 @echo off
 echo SpeedyWin NetworkFixer v1.0
 echo Use this if you are having network related speed issues with your computer.
+echo You should run this as administrator if possible. Right click, click "Run as administrator".
 pause
 ipconfig /flushdns
 netsh interface ipv4 set dns name=”Wi-Fi” static 8.8.8.8
